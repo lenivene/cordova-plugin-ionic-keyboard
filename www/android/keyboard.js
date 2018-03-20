92e0b2d123673da64b44479b5d17fc035c5a5c99
lenivene <lenivene@users.noreply.github.com>
2018-03-20 19:09:02 -0300

    Update keyboard.js

--- www/android/keyboard.js
+++ www/android/keyboard.js
@@ -1,67 +1,78 @@
+cordova.define("cordova-plugin-ionic-keyboard.keyboard", function(require, exports, module) {
 var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec'),
-    channel = require('cordova/channel');
+	utils = require('cordova/utils'),
+	exec = require('cordova/exec'),
+	channel = require('cordova/channel');
 
 
-var Keyboard = function () {};
+function Keyboard(){}
 
-Keyboard.fireOnShow = function (height) {
-    Keyboard.isVisible = true;
-    cordova.fireWindowEvent('keyboardDidShow', {
-        'keyboardHeight': height
-    });
+Keyboard.prototype.fireOnShow = function (height) {
+	Keyboard.isVisible = true;
+	cordova.fireWindowEvent('keyboardDidShow', {
+		'keyboardHeight': height
+	});
 };
 
-Keyboard.fireOnHide = function () {
-    Keyboard.isVisible = false;
-    cordova.fireWindowEvent('keyboardDidHide');
+Keyboard.prototype.fireOnHide = function () {
+	Keyboard.isVisible = false;
+	cordova.fireWindowEvent('keyboardDidHide');
 };
 
-Keyboard.fireOnHiding = function () {
-    cordova.fireWindowEvent('keyboardWillHide');
+Keyboard.prototype.fireOnHiding = function () {
+	cordova.fireWindowEvent('keyboardWillHide');
 };
 
-Keyboard.fireOnShowing = function (height) {
-    cordova.fireWindowEvent('keyboardWillShow', {
-        'keyboardHeight': height
-    });
+Keyboard.prototype.fireOnShowing = function (height) {
+	cordova.fireWindowEvent('keyboardWillShow', {
+		'keyboardHeight': height
+	});
 };
 
-Keyboard.hideKeyboardAccessoryBar = function (hide) {
-    exec(null, null, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
+Keyboard.prototype.hideKeyboardAccessoryBar = function (hide) {
+	exec(null, null, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
 };
 
-Keyboard.close = function () {
-    exec(null, null, "Keyboard", "close", []);
+Keyboard.prototype.close = function () {
+	exec(null, null, "Keyboard", "close", []);
 };
 
-Keyboard.show = function () {
-    exec(null, null, "Keyboard", "show", []);
+Keyboard.prototype.show = function () {
+	exec(null, null, "Keyboard", "show", []);
 };
 
-Keyboard.disableScroll = function (disable) {
-    console.warn("Keyboard.disableScroll() was removed");
+Keyboard.prototype.disableScroll = function (disable) {
+	console.warn("Keyboard.disableScroll() was removed");
 };
 
 channel.onCordovaReady.subscribe(function () {
-    exec(success, null, 'Keyboard', 'init', []);
+	exec(success, null, 'Keyboard', 'init', []);
 
-    function success(msg) {
-        var action = msg.charAt(0);
-        if (action === 'S') {
-            var keyboardHeight = parseInt(msg.substr(1));
-            Keyboard.fireOnShowing(keyboardHeight);
-            Keyboard.fireOnShow(keyboardHeight);
+	function success(msg) {
+		var action = msg.charAt(0);
+		if (action === 'S') {
+			var keyboardHeight = parseInt(msg.substr(1));
+			Keyboard.fireOnShowing(keyboardHeight);
+			Keyboard.fireOnShow(keyboardHeight);
 
-        } else if (action === 'H') {
-            Keyboard.fireOnHiding();
-            Keyboard.fireOnHide();
-        }
-    }
+		} else if (action === 'H') {
+			Keyboard.fireOnHiding();
+			Keyboard.fireOnHide();
+		}
+	}
 });
 
+Keyboard.prototype.isVisible = false;
 
-Keyboard.isVisible = false;
+Keyboard.install = function () {
+	if (!window.plugins) {
+		window.plugins = {};
+	}
 
-module.exports = Keyboard;
+	window.plugins.Keyboard = new Keyboard();
+
+	return window.plugins.Keyboard;
+};
+
+cordova.addConstructor(Keyboard.install);
+});
